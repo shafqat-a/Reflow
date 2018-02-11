129f5f027f2df4af8f9d2046ad6627496de7c29b
Shafqat Ahmed <shafqatahmed@gmail.com>
2018-02-11 15:15:55 +0600

    Updated Etl with json

--- Java/src/test/res/voted-unvoted-broadridge.json.js
+++ Java/src/test/res/voted-unvoted-broadridge.json.js
@@ -18,9 +18,6 @@
                 {name:"record_type", start: "0", length: "2"},
                 {name:"request_number", start:"2",length: "6"},
                 {name:"request_type", start: "17", length: "1"}
-                //,
-                //{name:"request_desc", start:"35", end: "75"}
-
             ], isValid : function (content) {
             if (content!=null && content.substr(0,2)=="01"){
                 return true;
